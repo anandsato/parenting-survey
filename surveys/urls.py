dd79a766e86461b917ed48cb264b4b7d878a1a95
Sato Jonas Bolin <bolin.jonas@gmail.com>
2015-06-10 00:28:06 +0200

    changed admin url

--- surveys/urls.py
+++ surveys/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'surveys.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tntsecretadmin/', include(admin.site.urls)),
     url(r'^$', 'submissions.views.profile', name='profile'),
     url(r'^thank-you/$', 'submissions.views.thank_you', name='thank_you'),
     url(r'^results/$', 'submissions.views.results', name='results'),
